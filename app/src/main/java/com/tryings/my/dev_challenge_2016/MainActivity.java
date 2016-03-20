4e460be739a636c9f360b9b339076f4ebe92d95e
igor-shaula <igor-shaula@gmail.com>
2016-03-20 20:23:36 +0200

    text field is set, snake behavior is next to be done

--- app/src/main/java/com/tryings/my/dev_challenge_2016/MainActivity.java
+++ app/src/main/java/com/tryings/my/dev_challenge_2016/MainActivity.java
@@ -6,62 +6,111 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private char food = '$';
-    private char foodBonus = '^';
-    private char foodSpeedSlow = '-';
-    private char foodSpeedUp = '+';
+    private char food = '$'; // +1 to length
+    private char foodBonus = '^'; // +2 to length
+    private char foodMinus = '*'; // -1 from length
+    private char foodSpeedSlow = '-'; // slows by 25%
+    private char foodSpeedUp = '+'; // speeds up by 25%
     private Snake snake;
 
-    private int mainFieldWidth, mainFieldHeight;
-    private int mainFieldSymbolsInLine, mainFieldLineCount;
+    private int fieldPixelWidth, fieldPixelHeight; // in pixels
+    private int mainFieldSymbolsInLine, mainFieldLineCount; // in items - for arrays \
 
-    private AppCompatTextView tvMainField;
+    private ArrayList<char[]> mainCharArrayList;
+
+    private AppCompatTextView actvMainField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvMainField = (AppCompatTextView) findViewById(R.id.tvMainField);
+        actvMainField = (AppCompatTextView) findViewById(R.id.tvMainField);
 
-        // 1 via getViewTreeObserver().addOnGlobalLayoutListener - works
-        tvMainField.getViewTreeObserver().addOnGlobalLayoutListener(
+        assert actvMainField != null;
+        actvMainField.getViewTreeObserver().addOnGlobalLayoutListener(
                 new ViewTreeObserver.OnGlobalLayoutListener() {
                     @Override
                     public void onGlobalLayout() {
-                        mainFieldWidth = tvMainField.getWidth();
-                        MyLog.d("mainFieldWidth " + mainFieldWidth);
-                        mainFieldHeight = tvMainField.getHeight();
-                        MyLog.d("mainFieldHeight " + mainFieldHeight);
+
+                        // getting current screen size in pixels \
+                        fieldPixelWidth = actvMainField.getWidth();
+                        MyLog.d("fieldPixelWidth " + fieldPixelWidth);
+
+                        FrameLayout flMain = (FrameLayout) findViewById(R.id.flMain);
+                        assert flMain != null;
+                        fieldPixelHeight = flMain.getHeight();
+//                        fieldPixelHeight = actvMainField.getHeight();
+                        MyLog.d("fieldPixelHeight " + fieldPixelHeight);
+
                         // now removing the listener - it's not needed any more \
                         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
-                            tvMainField.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                            actvMainField.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                         else
-                            tvMainField.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                            actvMainField.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+
+                        // moving on to separate method \
+                        prepareTextField();
                     }
                 });
 
+        // TODO: 20.03.2016 make method to set food and bonuses initially on screen \
+
+        // TODO: 20.03.2016 make method to randomly choose initial direction \
+
+        // TODO: 20.03.2016 make method to add the snake to the field \
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void prepareTextField() {
 
-        // task 1 - get exact width and height of the main text field - not in pixels, but in symbols \
+        // here we get pixel width of a single symbol - initial TextView has only one symbol \
+        actvMainField.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        int measuredSymbolWidth = actvMainField.getMeasuredWidth();
+        MyLog.i("measuredSymbolWidth " + measuredSymbolWidth);
 
-        // 2 - here we get pixel sizes of a single symbol \
-        tvMainField.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        int measuredWidth = tvMainField.getMeasuredWidth();
-        int measuredHeight = tvMainField.getMeasuredHeight();
-        MyLog.i("measuredWidth " + measuredWidth);
-        MyLog.i("measuredHeight " + measuredHeight);
-
-        mainFieldSymbolsInLine = mainFieldWidth / measuredWidth;
+        // getting our first valuable parameter - the size of main array \
+        mainFieldSymbolsInLine = fieldPixelWidth / measuredSymbolWidth;
         MyLog.i("mainFieldSymbolsInLine " + mainFieldSymbolsInLine);
 
+        // clearing the text field to properly initialize it for game \
+        actvMainField.setText(null);
 
+        // now preparing our model and initializing text field \
+        int i = 0, measuredTextHeight;
+        mainCharArrayList = new ArrayList<>();
+        do {
+            // filling up out TextView to measure its lines and set initial state at once \
+            char[] charArray = new char[mainFieldSymbolsInLine + 1];
+            // setting end element for a new line on the next array \
+            charArray[mainFieldSymbolsInLine] = '\n';
+            // setting other elements to their default values \
+            for (int j = 0; j < mainFieldSymbolsInLine; j++) {
+                charArray[j] = '+';
+            }
+            // now single char array is ready and has to be added to the list \
+            mainCharArrayList.add(i, charArray);
+            MyLog.i("added " + new String(mainCharArrayList.get(i)));
+
+            String previousText = actvMainField.getText().toString();
+            String newText = previousText + new String(mainCharArrayList.get(i));
+            actvMainField.setText(newText);
+            MyLog.i("newText \n" + newText);
+
+            i++;
+
+            actvMainField.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            measuredTextHeight = actvMainField.getMeasuredHeight();
+            MyLog.i("measuredTextHeight " + measuredTextHeight);
+
+        } while (measuredTextHeight <= fieldPixelHeight);
+
+        mainFieldLineCount = i;
+        MyLog.i("mainFieldLineCount " + mainFieldLineCount);
     }
 
     @Override
